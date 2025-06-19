4b5f80626c9b9b2c2757540cfd4db343390b1871
thedavidmeister <thedavidmeister@gmail.com>
2025-06-19 21:03:51 +0400

    wip on nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Flake for development workflows.";
+
+  inputs = {
+    rainix.url = "github:rainprotocol/rainix";
+    rain.url = "github:rainlanguage/rain.cli";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, rainix, rain }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = rainix.pkgs.${system};
+      in rec
+      {
+        packages = {
+          rain-merkle-prelude = rainix.mkTask.${system} {
+            name = "rain-merkle-prelude";
+            body = ''
+              set -euxo pipefail
+
+              mkdir -p meta;
+              forge script --silent ./script/BuildAuthoringMeta.sol;
+              rain meta build \
+                -i <(cat ./meta/MerkleSubParserAuthoringMeta.rain.meta) \
+                -m authoring-meta-v2 \
+                -t cbor \
+                -e deflate \
+                -l none \
+                -o meta/MerkleWords.rain.meta \
+                ;
+            '';
+          };
+        } // rainix.packages.${system};
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            packages.rain-merkle-prelude
+            rain.defaultPackage.${system}
+          ];
+
+          shellHook = rainix.devShells.${system}.default.shellHook;
+          buildInputs = rainix.devShells.${system}.default.buildInputs;
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
+
+        };
+      }
+    );
+
+}
