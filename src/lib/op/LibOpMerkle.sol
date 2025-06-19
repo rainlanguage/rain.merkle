be7f25a6c6d4297ce1c57a7ee9ea1dcf7ea8d9c7
thedavidmeister <thedavidmeister@gmail.com>
2025-06-19 22:24:45 +0400

    wip on merkle op

---
+++ src/lib/op/LibOpMerkle.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity ^0.8.25;
+
+import {Operand} from "rain.interpreter.interface/interface/deprecated/IInterpreterV2.sol";
+import {MerkleProof} from "openzeppelin-contracts/utils/cryptography/MerkleProof.sol";
+
+library LibOpMerkle {
+    function integrity(Operand, uint256 inputs, uint256) internal pure returns (uint256, uint256) {
+        // Merkle proof requires dynamic proof inputs and produces 1 output.
+        return (inputs, 1);
+    }
+
+    function run(Operand, uint256[] memory inputs) internal pure returns (uint256[] memory) {
+        bytes32 root;
+        bytes32 leaf;
+        bytes32[] memory proof;
+
+        assembly ("memory-safe") {
+            // The first input is the root of the Merkle tree.
+            root := mload(add(inputs, 0x20))
+            // The second input is the leaf to verify.
+            leaf := mload(add(inputs, 0x40))
+            // The rest are the Merkle proof elements.
+            // Mutate the inputs array to be a proof elements array.
+            let proofLength := sub(mload(inputs), 2)
+            proof := add(inputs, 0x40)
+            mstore(proof, proofLength)
+        }
+
+        // Verify the Merkle proof.
+        uint256 isValid = MerkleProof.verify(proof, root, leaf) ? 1 : 0;
+        assembly ("memory-safe") {
+            // We can mutate inputs again to be outputs.
+            mstore(inputs, 1)
+            mstore(add(inputs, 0x20), isValid)
+        }
+        return inputs;
+    }
+}
---
+++ src/lib/parse/LibMerkleSubParser.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity ^0.8.25;
+
+import {AuthoringMetaV2} from "rain.interpreter.interface/interface/deprecated/IParserV1.sol";
+
+uint256 constant SUB_PARSER_WORD_MERKLE_PRICE = 0;
+
+uint256 constant SUB_PARSER_WORD_PARSERS_LENGTH = 1;
+
+library LibMerkleSubParser {
+    function authoringMetaV2() internal pure returns (bytes memory) {
+        AuthoringMetaV2[] memory meta = new AuthoringMetaV2[](SUB_PARSER_WORD_PARSERS_LENGTH);
+
+        meta[SUB_PARSER_WORD_MERKLE_PRICE] = AuthoringMetaV2(
+            "verify-merkle-proof",
+            "Verifies a Merkle proof according to Open Zeppelin 5.x logic. Returns true if the proof is valid (caller must ensure). First value is the merkle root. Second is the leaf. Tail are the proof values."
+        );
+        return abi.encode(meta);
+    }
+}
