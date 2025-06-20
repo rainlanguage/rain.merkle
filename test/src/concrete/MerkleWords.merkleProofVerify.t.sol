78f5a7f3bc0cb16834a0f67a56ac9a06c6adaae0
thedavidmeister <thedavidmeister@gmail.com>
2025-06-20 14:52:08 +0400

    test for merkle verify

---
+++ test/src/concrete/MerkleWords.merkleProofVerify.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity =0.8.25;
+
+import {MerkleWords} from "src/concrete/MerkleWords.sol";
+import {OpTest} from "rain.interpreter/../test/abstract/OpTest.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+contract MerkleWordsMerkleProofVerifyTest is OpTest {
+    using Strings for address;
+    using Strings for uint256;
+
+    function testMerkleWordsMerkleProofVerifyHappy() external {
+        MerkleWords merkleWords = new MerkleWords();
+
+        uint256 root = 0xd4dee0beab2d53f2cc83e567171bd2820e49898130a22622b10ead383e90bd77;
+        uint256 proof = 0xb92c48e9d7abe27fd8dfd6b5dfdbfb1c9a463f80c712b66f3a5180a090cccafc;
+        uint256 leaf = uint256(
+            keccak256(
+                bytes.concat(keccak256(abi.encode(0x1111111111111111111111111111111111111111, 5000000000000000000)))
+            )
+        );
+
+        uint256[] memory expectedStack = new uint256[](4);
+        expectedStack[0] = 1;
+        expectedStack[1] = proof;
+        expectedStack[2] = leaf;
+        expectedStack[3] = root;
+
+        checkHappy(
+            bytes(
+                string.concat(
+                    "using-words-from ",
+                    address(merkleWords).toHexString(),
+                    "\n",
+                    "root: ",
+                    root.toHexString(),
+                    ",",
+                    "leaf: ",
+                    leaf.toHexString(),
+                    ",",
+                    "proof: ",
+                    proof.toHexString(),
+                    ",",
+                    " _: merkle-proof-verify(root leaf proof);"
+                )
+            ),
+            expectedStack,
+            "merkle-proof-verify(root leaf proof)"
+        );
+    }
+}
